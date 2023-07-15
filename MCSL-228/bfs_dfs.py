c6ca24c8f303d1dd87450c9fc35b27401bb60388
Kuntal Das <25ckuntaldas@gmail.com>
2023-07-15 14:03:26 +0530

    idfs

--- MCSL-228/bfs_dfs.py
+++ MCSL-228/bfs_dfs.py
@@ -3,12 +3,16 @@ from collections import defaultdict
 class Graph:
     def __init__(self):
         self.graph = defaultdict(list)
+        # self.CLOSE_visited = set()
+
+    # def reset_visited_set(self):
+    #     self.CLOSE_visited = set()
 
     def add_edge(self, u, v):
         self.graph[u].append(v)
 
     def bfs(self, start_node, goal_node):
-        print("\nBFS node traversal: ", end= " ")
+        print("BFS node traversal: ", end= " ")
         CLOSE_visited = set()
         OPEN_queue = []
 
@@ -19,15 +23,18 @@ class Graph:
             node = OPEN_queue.pop(0)
             print(node, end=" ")
             if node == goal_node:
+                return True
                 break
 
             for adjacent_node in self.graph[node]:
                 if adjacent_node not in CLOSE_visited:
                     CLOSE_visited.add(adjacent_node)
                     OPEN_queue.append(adjacent_node)
+        
+        return False
 
     def dfs(self, start_node, goal_node):
-        print("\nDFS node traversal: ", end=" ")
+        print("DFS node traversal: ", end=" ")
         CLOSE_visited = set()
         OPEN_stack = []
 
@@ -38,16 +45,66 @@ class Graph:
             node = OPEN_stack.pop(0)
             print(node, end=" ")
             if node == goal_node:
+                return True
                 break
 
             for adjacent_node in self.graph[node]:
                 if adjacent_node not in CLOSE_visited:
                     CLOSE_visited.add(adjacent_node)
                     OPEN_stack.insert(0, adjacent_node)
+        return False
 
-if __name__ == '__main__':
-    # Example usage
-    g = Graph()
+
+    def _dfs_recursive(self, current_node, goal_node, CLOSE_visited):
+        if current_node == goal_node:
+            print(current_node, end=" ")
+            return True
+        
+        # if start_node in CLOSE_visited:
+        #     return False
+        
+        print(current_node, end=" ")
+        CLOSE_visited.add(current_node)
+
+        for adjacent_node in self.graph[current_node]:
+            if adjacent_node not in CLOSE_visited and self._dfs_recursive(adjacent_node, goal_node, CLOSE_visited):
+                return True
+        
+        return False
+    
+    def dfs_recursive(self, start_node, goal_node):
+        CLOSE_visited = set()
+        print("Recursive DFS node traversal: ", end=" ")
+        return self._dfs_recursive(start_node, goal_node, CLOSE_visited)
+
+
+    def iddfs(self, start_node, target_node, max_depth):
+        print(f"Iterative DFS node traversal with depth[{max_depth}]: ", end=" ")
+        for depth in range(1, max_depth + 1):
+            CLOSE_visited = set()
+            print(f"\nDepth: {depth}:", end=" ")
+            if self.dfs_by_depth(start_node, target_node, depth, CLOSE_visited):
+                return True
+        return False
+
+    def dfs_by_depth(self, current_node, target_node, depth, CLOSE_visited):
+        if depth <= 0:
+            return False
+        
+        if current_node == target_node:
+            print(current_node, end=" ")
+            return True
+
+        print(current_node, end=" ")
+        CLOSE_visited.add(current_node)
+
+        for neighbor in self.graph[current_node]:
+            if neighbor not in CLOSE_visited and self.dfs_by_depth(neighbor, target_node, depth - 1, CLOSE_visited):
+                return True
+
+        return False
+
+def Graph1(g):
     g.add_edge(1, 2) 
     g.add_edge(1, 3) 
     g.add_edge(1, 4) 
@@ -62,9 +119,61 @@ if __name__ == '__main__':
     g.add_edge(8, 5)
     g.add_edge(9, 6)
     g.add_edge(9, 8)
+    return 1, 8
 
-    start_node, goal_node = 1, 8
+def Graph2(g):
+    g.add_edge('A', 'B')
+    g.add_edge('A', 'C')
+    g.add_edge('A', 'D')
+
+    g.add_edge('B', 'E')
+    g.add_edge('B', 'F')
+
+    g.add_edge('C', 'G')
+    g.add_edge('C', 'H')
+
+    g.add_edge('D', 'I')
+    g.add_edge('D', 'J')
+
+    g.add_edge('E', 'K')
+    g.add_edge('E', 'L')
+
+    g.add_edge('F', 'L')
+    g.add_edge('F', 'M')
+
+    g.add_edge('G', 'N')
+
+    g.add_edge('H', 'O')
+    g.add_edge('H', 'P')
+    
+    g.add_edge('I', 'P')
+    g.add_edge('I', 'Q')
+    
+    g.add_edge('J', 'R')
+
+    g.add_edge('K', 'S')
+    g.add_edge('L', 'T')
+
+    g.add_edge('P', 'U')
+
+    return 'A', 'M'
+
+if __name__ == '__main__':
+    g = Graph()
+
+    start_node, goal_node = Graph2(g)
+
     print("Traversal starting node", start_node)
     print("Traversal goal node", goal_node)
-    g.bfs(start_node, goal_node)
-    g.dfs(start_node, goal_node)
+
+    if g.bfs(start_node, goal_node): print("\t[GOAL FOUND]")
+    else: print("\t[GOAL NOT FOUND]")
+        
+    if g.dfs(start_node, goal_node): print("\t[GOAL FOUND]")
+    else: print("\t[GOAL NOT FOUND]")
+
+    if g.dfs_recursive(start_node, goal_node): print("\t[GOAL FOUND]")
+    else: print("\t[GOAL NOT FOUND]")  
+
+    if g.iddfs(start_node, goal_node, 5): print("\t[GOAL FOUND]")
+    else: print("\t[GOAL NOT FOUND]")
